cd7c5b2ad3f05ffe9eb67bb78221b04dc9bc743f
joaoaugusto-dev <joao.a.freitas@sou.unifeob.edu.br>
2025-09-13 11:11:50 -0300

    Início da estrutura de tomada de decisões - apresentação do IF e ELSE

---
+++ pj05_dart_estudodetomadadedecisoes/bin/pj05_dart_estudodetomadadedecisoes.dart
@@ -0,0 +1,11 @@
+void main() {
+  int idade = 18;
+
+  if (idade >= 18) {
+    print("É maior de idade.");
+    print("Olá!");
+  } else {
+    print("É menor de idade.");
+    print("Tchau!");
+  }
+}
