42717091ee54cf4b405284da8a6b181401c7fd87
joaoaugusto-dev <joao.a.freitas@sou.unifeob.edu.br>
2025-10-04 17:56:18 -0300

    Introdução a listas

---
+++ pj20_dar_lsitas.dart
@@ -0,0 +1,17 @@
+void main() {
+  List<String> nomes = ["Daniel", "Ciolfi", "Henrique"];
+
+  print(nomes);
+
+  List<int> idades = [12, 32, 56, 78];
+
+  print(idades);
+
+  print(idades.length); //Imprime a quantidade de itens em uma lista
+  print(idades.first); //Imprime o primeiro item da lsita
+  print(idades.last); //Imprime o último item da lista
+  print(idades[0]); //
+  print(idades[1]); // // Imprime um item especifico da lista
+  print(idades[2]); //
+  print(idades[3]); //
+}
