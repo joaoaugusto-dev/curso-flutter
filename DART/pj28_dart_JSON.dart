72181b2eff872f5af09d1418fb9903e789c205f0
joaoaugusto-dev <joao.a.freitas@sou.unifeob.edu.br>
2025-10-05 19:54:36 -0300

    Introdução ao tratamento e interpretação de JSON

---
+++ DART/pj28_dart_JSON.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+void main() {
+  Map<String, dynamic> dados = json.decode(dadosDoUsuario());
+
+  print(dados["cursos"][0]["dificuldade"]);
+}
+
+String dadosDoUsuario() {
+  return """
+{
+  "nome": "Daniel Henrique",
+  "sobrenome": "Ciolfi",
+  "idade": 50,
+  "casado": false,
+  "altura": 1.82,
+  "cursos": [
+    {
+      "nome": "Dart",
+      "dificuldade": 1
+    },
+    {
+      "nome": "Flutter",
+      "dificuldade": 2
+    }
+  ],
+  "endereco": {
+    "cidade": "Campinas",
+    "pais": "Brasil",
+    "numero": 100
+  }
+}
+""";
+}
