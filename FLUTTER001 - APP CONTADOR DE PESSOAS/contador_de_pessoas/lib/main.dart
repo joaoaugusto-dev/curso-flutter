8ba647279e09366ba6d76754605c011253f7754a
joaoaugusto-dev <joao.a.freitas@sou.unifeob.edu.br>
2025-10-18 16:04:38 -0300

    Imagem de fundo e pequenas evoluções de design

--- FLUTTER001 - APP CONTADOR DE PESSOAS/contador_de_pessoas/lib/main.dart
+++ FLUTTER001 - APP CONTADOR DE PESSOAS/contador_de_pessoas/lib/main.dart
@@ -10,7 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const HomePage());
+    return MaterialApp(
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
@@ -31,84 +34,127 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.red,
       //coluna
-      body: Column(
-        //centralizacao
-        mainAxisAlignment:
-            MainAxisAlignment.center,
-        children: [
-          const Text(
-            "Pode entrar!",
-            style: TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-              fontWeight: FontWeight.w700,
-              //letterSpacing: 1,
-              //backgroundColor: Colors.white,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              "assets/images/image.jpg",
             ),
+            fit: BoxFit.cover,
           ),
-          Padding(
-            padding: EdgeInsets.fromLTRB(
-              0,
-              32,
-              0,
-              32,
-            ),
-            child: Text(
-              "0",
-              style: TextStyle(
-                fontSize: 100,
-                color: Colors.white,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(
+            60,
+            220,
+            60,
+            220,
+          ),
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(
+                210,
+                0,
+                0,
+                0,
+              ),
+              borderRadius: BorderRadius.circular(
+                32,
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment:
-                MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(100, 100),
-                  foregroundColor: Colors
-                      .black, //primary foi trocado pelo foregroundColor
-                  shape: RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(16),
-                  ),
-                ),
-                child: Text(
-                  "Saiu",
+            child: Column(
+              //centralizacao
+              mainAxisAlignment:
+                  MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Pode entrar!",
                   style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    //letterSpacing: 1,
+                    //backgroundColor: Colors.white,
                   ),
                 ),
-              ),
-              const SizedBox(width: 32),
-              TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(100, 100),
-                  foregroundColor: Colors
-                      .black, //primary foi trocado pelo foregroundColor
-                  shape: RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(16),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(
+                    0,
+                    32,
+                    0,
+                    32,
+                  ),
+                  child: Text(
+                    "0",
+                    style: TextStyle(
+                      fontSize: 100,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-                child: Text(
-                  "Saiu",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                  ),
+                Row(
+                  mainAxisAlignment:
+                      MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: decrement,
+                      style: TextButton.styleFrom(
+                        backgroundColor:
+                            Colors.white,
+                        fixedSize: const Size(
+                          100,
+                          100,
+                        ),
+                        foregroundColor: Colors
+                            .black, //primary foi trocado pelo foregroundColor
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(
+                                16,
+                              ),
+                        ),
+                      ),
+                      child: Text(
+                        "Saiu",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 32),
+                    TextButton(
+                      onPressed: increment,
+                      style: TextButton.styleFrom(
+                        backgroundColor:
+                            Colors.white,
+                        fixedSize: const Size(
+                          100,
+                          100,
+                        ),
+                        foregroundColor: Colors
+                            .black, //primary foi trocado pelo foregroundColor
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(
+                                16,
+                              ),
+                        ),
+                      ),
+                      child: Text(
+                        "Entrou",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
