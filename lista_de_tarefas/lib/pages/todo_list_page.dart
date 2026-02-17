4021425fbbc4831c5554e418a1ca4f818339f98b
joaoaugusto-dev <joao.a.freitas@sou.unifeob.edu.br>
2026-02-17 17:34:26 -0300

    Adição dos botões inferiores e espaçamentos

--- lista_de_tarefas/lib/pages/todo_list_page.dart
+++ lista_de_tarefas/lib/pages/todo_list_page.dart
@@ -9,25 +9,47 @@ class TodoListPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Row(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
             children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Adicione uma tarefa",
-                    hintText: "Ex. Estudar Flutter",
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Adicione uma tarefa",
+                        hintText: "Ex. Estudar Flutter",
+                      ),
+                    ),
                   ),
-                ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff00d7f3),
+                      padding: const EdgeInsets.all(12),
+                    ),
+                    child: Icon(Icons.add, size: 30, color: Colors.white),
+                  ),
+                ],
               ),
-              SizedBox(width: 8),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xff00d7f3),
-                  padding: const EdgeInsets.all(12),
-                ),
-                child: Icon(Icons.add, size: 30, color: Colors.white),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(child: Text("Você possui 0 tarefas pendentes.")),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff00d7f3),
+                      padding: const EdgeInsets.all(12),
+                    ),
+                    child: Text(
+                      "Limpar tudo",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
