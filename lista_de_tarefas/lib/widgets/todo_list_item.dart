4c19fc5f31cb5fd54837ffebdb3ca965db054967
joaoaugusto-dev <joao.a.freitas@sou.unifeob.edu.br>
2026-02-19 09:55:35 -0300

    Adição do pacote slidabble e configuração de ícone para deletar

--- lista_de_tarefas/lib/widgets/todo_list_item.dart
+++ lista_de_tarefas/lib/widgets/todo_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:lista_de_tarefas/models/todo.dart';
 
@@ -9,22 +10,38 @@ class TodoListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(14),
-        color: Colors.grey[200],
-      ),
-      margin: const EdgeInsets.symmetric(vertical: 2),
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        extentRatio: 0.25,
         children: [
-          Text(
-            DateFormat("dd/mm/yyyy - hh:mm").format(todo.dateTime),
-            style: TextStyle(fontSize: 12),
+          SlidableAction(
+            onPressed: (context) {},
+            backgroundColor: Colors.red,
+            icon: Icons.delete,
+            label: 'Deletar',
+            borderRadius: BorderRadius.circular(8),
           ),
-          Text(todo.title, style: TextStyle(fontSize: 16, fontWeight: .w600)),
         ],
+      ),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          color: Colors.grey[200],
+        ),
+        margin: const EdgeInsets.symmetric(vertical: 2),
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              DateFormat("dd/mm/yyyy - hh:mm").format(todo.dateTime),
+              style: TextStyle(fontSize: 12),
+            ),
+            Text(todo.title, style: TextStyle(fontSize: 16, fontWeight: .w600)),
+          ],
+        ),
       ),
     );
   }
