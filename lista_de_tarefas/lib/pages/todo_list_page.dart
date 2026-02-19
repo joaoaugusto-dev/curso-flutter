a357ddcef28087aa9fbcb55491a1833382540b4f
joaoaugusto-dev <joao.a.freitas@sou.unifeob.edu.br>
2026-02-19 10:28:49 -0300

    Adição de snackbar de deleção, incluindo função de desfazer, que armazena a última tarefa excluída, e se necessário, a insere novamente na lsita de toos, na mesma posição em que estava

--- lista_de_tarefas/lib/pages/todo_list_page.dart
+++ lista_de_tarefas/lib/pages/todo_list_page.dart
@@ -11,6 +11,8 @@ class TodoListPage extends StatefulWidget {
 
 class _TodoListPageState extends State<TodoListPage> {
   List<Todo> todos = [];
+  Todo? deletedTodo;
+  int? deletedTodoPos;
 
   final TextEditingController todoController = TextEditingController();
 
@@ -97,8 +99,32 @@ class _TodoListPageState extends State<TodoListPage> {
   }
 
   void onDelete(Todo todo) {
+    deletedTodo = todo;
+    deletedTodoPos = todos.indexOf(todo);
+
     setState(() {
       todos.remove(todo);
     });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          "Tarefa ${todo.title} foi removida com sucesso!",
+          style: TextStyle(color: Color(0xff060708)),
+        ),
+        backgroundColor: Colors.white,
+        action: SnackBarAction(
+          label: "Desfazer",
+          onPressed: () {
+            setState(() {
+              todos.insert(deletedTodoPos!, deletedTodo!);
+            });
+          },
+          textColor: Color(0xff00d7f3),
+        ),
+        duration: const Duration(seconds: 5),
+      ),
+    );
   }
 }
